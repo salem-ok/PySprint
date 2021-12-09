bb32408fb851e479ef06e2ca4f496905b50c8496
mike.carton@posteo.net <mike.carton@posteo.net>
2021-12-09 19:00:29 +0000

    Reverse Merge of pull request #65

--- pysprint.py
+++ pysprint.py
@@ -5,7 +5,6 @@ from pygame import gfxdraw
 import numpy as np
 import pysprint_car
 import pysprint_tracks
-import pysprint_control_methods as pysprint_cm
 import random
 import json
 
@@ -749,8 +748,38 @@ green_car_sprites = {
         15:pygame.image.load('Assets/GreenCar15.png').convert_alpha()
 }
 
-control_methods = pysprint_cm.control_methods
+keyboard_1 = {
+    'ACCELERATE': pygame.K_RCTRL,
+    'LEFT': pygame.K_LEFT,
+    'RIGHT': pygame.K_RIGHT,
+    'METHOD': 'KEYBOARD 1'
+}
 
+keyboard_2 = {
+    'ACCELERATE': pygame.K_LCTRL,
+    'LEFT': pygame.K_x,
+    'RIGHT': pygame.K_c,
+    'METHOD': 'KEYBOARD 2'
+}
+
+joystick_1 = {
+    'METHOD': 'JOYSTICK 1'
+}
+
+joystick_2 = {
+    'METHOD': 'JOYSTICK 2'
+}
+
+joystick_3 = {
+    'METHOD': 'JOYSTICK 3'
+}
+
+joystick_4 = {
+    'METHOD': 'JOYSTICK 4'
+}
+
+control_methods = [keyboard_1, keyboard_2, joystick_1, joystick_2,
+                   joystick_3, joystick_4]
 
 def screen_fadeout():
     for frame in range (0,len(transition_dots)):
